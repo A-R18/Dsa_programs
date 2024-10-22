2bef103c2f134c6861bdb8ae4639907f31328252
Abdullah Rashid <gamchanger18@gmail.com>
2024-10-22 20:52:10 +0500

    Add these changes

--- alert.js
+++ alert.js
@@ -1,1 +1,28 @@
-alert("This is the main file") ;
+
+const nbClr = document.getElementById('nbr');
+
+nbClr.firstElementChild.firstElementChild.style.color = 'red';
+
+nbClr.firstElementChild.lastElementChild.style.color = 'orange';
+
+
+const listITems = document.getElementsByTagName('li');
+console.log(listITems);
+const litmz = Array.from(listITems);
+litmz.forEach(li => {
+
+});
+// let a =['red', 'blue', 'green', 'purple', 'cyan', 'orange', 'beige', 'black', 'grey'];
+let i = 0;
+
+nbClr.addEventListener('click', function () {
+    setInterval(() => {
+
+        nbClr.style.backgroundColor = a[i];
+        i++
+        if (i >= a.length) {
+            i = 0;
+        }
+    }, 300);
+
+});
